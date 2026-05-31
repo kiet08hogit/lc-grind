775dc273b014bc1615112fd195e39ce523aa1eb0
Nguyen Tuan Kiet Ho <hokiet.nguyen08@gmail.com>
2026-05-30 20:30:34 -0500

    Add: partition-equal-subset-sum - submission-2

---
+++ Data Structures & Algorithms/partition-equal-subset-sum/submission-2.py
@@ -0,0 +1,22 @@
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        dp={}
+        # skip or take then add the path to dp and return backtracking
+        if sum(nums) % 2 != 0:
+            return False
+        total= sum(nums)
+        target= total // 2
+        def dfs(index, curr_sum):
+            if curr_sum == target:
+                return True
+            if curr_sum > target:
+                return False
+            if index == len(nums):
+                return False
+            if (index,curr_sum) in dp:
+                return dp[(index,curr_sum)]
+            take_turn= dfs(index+1,curr_sum + nums[index])
+            skip_turn= dfs(index+1,curr_sum)
+            dp[(index,curr_sum)]= take_turn or skip_turn
+            return dp[(index,curr_sum)]
+        return dfs(0,0)
